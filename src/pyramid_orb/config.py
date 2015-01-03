e85898ab2b28443d3825a7871902ee7317d8200b
Eric Hulser <eric.hulser@projexsoftware.com>
2015-01-02 18:34:16 -0800

    removed unused routes, supporting the trailing slash methods

--- src/pyramid_orb/config.py
+++ src/pyramid_orb/config.py
@@ -5,19 +5,10 @@ def add_routes(config, model, base=None):
         base = projex.text.pluralize(projex.text.underscore(model.schema().name()))
 
     # create the index route
-    config.add_route(base, base)
+    config.add_route(base, base + '/')
 
     # create the CRUD routes for the web
-    config.add_route('%s.create' % base, '%s/create' % base)
-    config.add_route('%s.show' % base, '%s/{id:\d+}' % base)
-    config.add_route('%s.edit' % base, '%s/{id:\d+}/edit' % base)
-    config.add_route('%s.remove' % base, '%s/{id:\d+}/remove' % base)
-
-    # create RESTFUL CRUD routes
-    config.add_route('api.%s' % base, '%s.json' % base, request_method='GET')
-    config.add_route('api.%s.create' % base, '%s.json' % base, request_method='POST')
-    config.add_route('api.%s.get' % base, '%s/{id:\d+}.json' % base, request_method='GET')
-    config.add_route('api.%s.update' % base, '%s/{id:\d+}.json' % base, request_method='PUT')
-    config.add_route('api.%s.delete' % base, '%s/{id:\d+}.json' % base, request_method='DELETE')
-    config.add_route('api.%s.delete_many' % base, '%s.json' % base, request_method='DELETE')
-
+    config.add_route('%s.create' % base, '%s/create/' % base)
+    config.add_route('%s.show' % base, '%s/{id:\d+}/' % base)
+    config.add_route('%s.edit' % base, '%s/{id:\d+}/edit/' % base)
+    config.add_route('%s.remove' % base, '%s/{id:\d+}/remove/' % base)
